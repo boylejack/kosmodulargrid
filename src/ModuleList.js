5ad8f37d18f0bceb6ff621483daf907647702386
Jack Boyle <jackmatthewboyel@gmail.com>
2021-09-23 22:45:28 +0100

    fixed maker filtering bug

--- src/ModuleList.js
+++ src/ModuleList.js
@@ -4,21 +4,20 @@ import { Tag } from 'primereact/tag';
 
 export const ModuleList = (props) => {
   const { modules, makers, selectedFilters } = props;
-
   return (
     <div className="p-grid">
       {modules
       .filter((module) => !selectedFilters.function || selectedFilters.function.length === 0 || selectedFilters.function.filter((fctn) => module.function.indexOf(fctn) >= 0).reduce((p,c) => p || c, false))
       .filter((module) => !selectedFilters.type || selectedFilters.type.length === 0 || selectedFilters.type.filter((t) => module.type.indexOf(t) >= 0).reduce((p,c) => p || c, false))
       .filter((module) => !selectedFilters.width || selectedFilters.width.length === 0 || selectedFilters.width.indexOf(module.width) >= 0)
-      .filter((module) => !selectedFilters.makerId || selectedFilters.makerId.length === 0 || selectedFilters.makerId.indexOf(module.makerId) >= 0)
+      .filter((module) => !selectedFilters.maker || selectedFilters.maker.length === 0 || selectedFilters.maker.indexOf(module.makerId) >= 0)
       .map(module => {
         return (
           <div className="p-col-12 p-md-6 p-lg-4" key={module.id}>
             <Card>
               <div className="p-grid" style={{height: "350px"}}>
                 <div className="p-col-6">
-                  <img style={{maxWidth: "100%", maxHeight: "100%", textAlign:'center'}} src={module.imageLink} alt={`${module.name} - ${module.description}`} />
+                  <img style={{maxWidth: "100%", maxHeight: "300px", textAlign:'center'}} src={module.imageLink} alt={`${module.name} - ${module.description}`} />
                 </div>
                 <div className="p-d-flex p-col-6 p-flex-column p-jc-between">
                   <div>
